9f058bb99e6447bf26fd367259e214fbf2672196
shisijin <784372659@qq.com>
2025-03-20 15:34:52 +0800

    1

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,3 +1,44 @@
+/**
+ * 示例1
+ * {
+ * "name": "nevi"
+ *  "age":18
+ * }
+ * https://helloworld-5lo.pages.dev/helloworld?name=zs
+ * @param context
+ * @returns {Promise<Response>}
+ */
+export async function onRequest(context) {
+    // 确保处理 POST 请求
+    if (context.request.method === "POST") {
+        try {
+            // 解析 JSON 数据
+            const body = await context.request.json();
+            // const name = body.name;  // 获取 JSON 中的 user 字段
+
+            // 返回包含 user 参数的响应
+            return new Response(`Hello ${JSON.stringify(body)}`);
+        } catch (error) {
+            return new Response("Invalid JSON or Error parsing JSON", { status: 400 });
+        }
+    }else if (context.request.method === "GET") {
+        const url = new URL(context.request.url);  // 获取请求 URL
+        const name = url.searchParams.get('name');  // 提取查询参数 `name`
+
+        // 返回包含 user 参数的响应
+        if (name) {
+            return new Response(name);
+        } else {
+            return new Response("User parameter is missing", { status: 400 });
+        }
+    }
+
+    // 如果不是 POST 请求，返回方法不允许的响应
+    return new Response("Method Not Allowed", { status: 405 });
+}
+
+
+
 // export function onRequest(context) {
 //     return handleRequest(context)
 // }
@@ -79,39 +120,3 @@
     // }
 // }
 
-/**
- * {
- * "name": "nevi"
- *  "age":18
- * }
- * @param context
- * @returns {Promise<Response>}
- */
-export async function onRequest(context) {
-    // 确保处理 POST 请求
-    if (context.request.method === "POST") {
-        try {
-            // 解析 JSON 数据
-            const body = await context.request.json();
-            // const name = body.name;  // 获取 JSON 中的 user 字段
-
-            // 返回包含 user 参数的响应
-            return new Response(`Hello ${JSON.stringify(body)}`);
-        } catch (error) {
-            return new Response("Invalid JSON or Error parsing JSON", { status: 400 });
-        }
-    }else if (context.request.method === "GET") {
-        const url = new URL(context.request.url);  // 获取请求 URL
-        const name = url.searchParams.get('name');  // 提取查询参数 `name`
-
-        // 返回包含 user 参数的响应
-        if (name) {
-            return new Response(name);
-        } else {
-            return new Response("User parameter is missing", { status: 400 });
-        }
-    }
-
-    // 如果不是 POST 请求，返回方法不允许的响应
-    return new Response("Method Not Allowed", { status: 405 });
-}
