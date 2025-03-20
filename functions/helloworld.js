aca72db36fd4f9c6161ef34c8c9a01f5e5bde095
shisijin <784372659@qq.com>
2025-03-20 14:44:38 +0800

    1

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -41,40 +41,41 @@ export function onRequest(context) {
 }
 
 async function handleRequest(context) {
-    try {
-        const response = await fetch('https://www.feishu.cn/flow/api/trigger-webhook/053dc9679ea562602cbe3c7e792d37ab', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json; charset=UTF-8',
-                // 如果需要认证可添加
-                // 'Authorization': 'Bearer your_token'
-            },
-            body: JSON.stringify({
-                model: "order"
-            })
-        });
-
-
-        // 检查HTTP状态码
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        // 解析飞书响应数据
-        const data = await response.json();
-        console.log('Success:', data);
-
-        // 返回一个 Cloudflare Worker 响应，包含飞书的响应内容
-        return new Response(JSON.stringify({ message: JSON.stringify(context.params) }), {
-            headers: { 'Content-Type': 'application/json' },
-        });
-
-    } catch (error) {
-        console.error('Error:', error);
-        // 处理错误逻辑并返回错误信息
-        return new Response(JSON.stringify({ success: false, message: error.message }), {
-            status: 500,
-            headers: { 'Content-Type': 'application/json' },
-        });
-    }
+    return new Response(`Hello ${context.params}`);
+    // try {
+    //     const response = await fetch('https://www.feishu.cn/flow/api/trigger-webhook/053dc9679ea562602cbe3c7e792d37ab', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json; charset=UTF-8',
+    //             // 如果需要认证可添加
+    //             // 'Authorization': 'Bearer your_token'
+    //         },
+    //         body: JSON.stringify({
+    //             model: "order"
+    //         })
+    //     });
+    //
+    //
+    //     // 检查HTTP状态码
+    //     if (!response.ok) {
+    //         throw new Error(`HTTP error! status: ${response.status}`);
+    //     }
+    //
+    //     // 解析飞书响应数据
+    //     const data = await response.json();
+    //     console.log('Success:', data);
+    //
+    //     // 返回一个 Cloudflare Worker 响应，包含飞书的响应内容
+    //     return new Response(JSON.stringify({ message: JSON.stringify(context.params) }), {
+    //         headers: { 'Content-Type': 'application/json' },
+    //     });
+    //
+    // } catch (error) {
+    //     console.error('Error:', error);
+    //     // 处理错误逻辑并返回错误信息
+    //     return new Response(JSON.stringify({ success: false, message: error.message }), {
+    //         status: 500,
+    //         headers: { 'Content-Type': 'application/json' },
+    //     });
+    // }
 }
