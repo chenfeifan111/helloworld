7addaa24b13d3e86fe656983fb1c9463925b69a9
shisijin <784372659@qq.com>
2025-03-20 14:27:34 +0800

    1

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,43 +1,10 @@
-// export function onRequest() {
-//     return handleRequest()
-// }
-//
-// async function handleRequest() {
-//     try {
-//         const response = await fetch('https://www.feishu.cn/flow/api/trigger-webhook/053dc9679ea562602cbe3c7e792d37ab', {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/json; charset=UTF-8',
-//                 // 如果需要认证可添加
-//                 // 'Authorization': 'Bearer your_token'
-//             },
-//             body: JSON.stringify({
-//                 model: "order"
-//             })
-//         });
-//
-//         // 检查HTTP状态码
-//         if (!response.ok) {
-//             throw new Error(`HTTP error! status: ${response.status}`);
-//         }
-//
-//         // 解析响应数据
-//         const data = await response.json();
-//         console.log('Success:', data);
-//         return data;
-//
-//     } catch (error) {
-//         console.error('Error:', error);
-//         // 处理错误逻辑
-//         throw error;
-//     }
-// }
+export function onRequest(context) {
+    return handleRequest(context)
+}
 
-
-
-export function handleRequest() {
+async function handleRequest(context) {
     try {
-        const response =  fetch('https://www.feishu.cn/flow/api/trigger-webhook/053dc9679ea562602cbe3c7e792d37ab', {
+        const response = await fetch('https://www.feishu.cn/flow/api/trigger-webhook/053dc9679ea562602cbe3c7e792d37ab', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json; charset=UTF-8',
@@ -48,14 +15,14 @@ export function handleRequest() {
                 model: "order"
             })
         });
-
+        console.log(context.params)
         // 检查HTTP状态码
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
 
         // 解析响应数据
-        const data =  response.json();
+        const data = await response.json();
         console.log('Success:', data);
         return data;
 
@@ -65,3 +32,4 @@ export function handleRequest() {
         throw error;
     }
 }
+
